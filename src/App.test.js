dfb94f97a5ca3b40212318e36c55da314b8d47f9
Bernardo <bernardo.bt@gmail.com>
2021-08-26 08:20:57 -0300

    feature: criação de testes para funções utilizadas em app

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import App, { calcularNovoSaldo } from './App'
 
 describe('Componente Principal', () => {
 
@@ -24,6 +24,45 @@ describe('Componente Principal', () => {
 
             const botao = screen.getByText('Realizar operação') // é case sensitive
             expect(botao).toBeInTheDocument();
+        })
+        
+    })
+
+
+    describe('Quando realizo uma transação', () => {
+        test('que é um SAQUE, o valor vai diminuir', () => {
+            
+            let valores = {
+                transacao: 'saque',
+                valor: '50'
+            };
+            const saldo = 150;
+            const novoSaldo = calcularNovoSaldo(valores, 150);
+
+            expect(novoSaldo).toBe(100);
+
+
+            valores = {
+                transacao: 'saque',
+                valor: '200'
+            }
+
+            const novoSaldoNegativo = calcularNovoSaldo(valores, 150);
+
+            expect(novoSaldoNegativo).toBe(-50);
+        })
+
+        test('que é um DEPOSITO, o valor vai aumentar', () => {
+            
+            const valores = {
+                transacao: 'deposito',
+                valor: '150'
+            };
+            const saldo = 150;
+            const novoSaldo = calcularNovoSaldo(valores, saldo);
+
+            expect(novoSaldo).toBe(300);
+
         })
         
     })
