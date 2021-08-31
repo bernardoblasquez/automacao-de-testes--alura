609cd25c8808b287ea96876ad4f0544a8841ceac
Bernardo <bernardo.bt@gmail.com>
2021-08-31 07:09:32 -0300

    feature: criação de teste com snapshot

---
+++ src/transacoes/trasacao.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Transacao from './Transacao';
+
+describe('Componente de Transação do extrato', () => {
+    
+    test('O spnapshot do comonente sempre deve permanecer o mesmo', () => {
+        const { container } = render(<Transacao
+            data = "08/09/2020"
+            tipo = "saque"
+            valor = "20.00"
+        />);
+
+        expect(container).toMatchSnapshot();
+    })
+    
+})
