b74693f2c6ac83d9e3d7a3fdce76965836befba7
Bernardo <bernardo.bt@gmail.com>
2021-08-31 07:51:56 -0300

    fix: correção de teste

--- src/transacoes/trasacao.test.js
+++ src/transacoes/trasacao.test.js
@@ -4,14 +4,14 @@ import Transacao from './Transacao';
 
 describe('Componente de Transação do extrato', () => {
     
-    test('O spnapshot do comonente sempre deve permanecer o mesmo', () => {
+    test('O spnapshot do componente sempre deve permanecer o mesmo', () => {
         const { container } = render(<Transacao
             data = "08/09/2020"
             tipo = "saque"
             valor = "20.00"
         />);
 
-        expect(container).toMatchSnapshot();
+        expect(container.firstChild).toMatchSnapshot();
     })
     
 })
